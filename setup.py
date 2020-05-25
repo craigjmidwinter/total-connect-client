40eebfee72fecdb0dbeb6759b1085f1297b1dd4e
Austin Mroczek <austin@mroczek.org>
2020-05-25 12:01:44 -0700

    Bump to 0.55.1

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 setup(
     name="total_connect_client",
     py_modules=["total_connect_client"],
-    version="0.55",
+    version="0.55.1",
     description="Interact with Total Connect 2 alarm systems",
     author="Craig J. Midwinter",
     author_email="craig.j.midwinter@gmail.com",
