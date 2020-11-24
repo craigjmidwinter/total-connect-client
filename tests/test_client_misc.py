d20470681c2eaaf3d8bd3b8b7d489ee691201e55
Austin Mroczek <austin@mroczek.org>
2020-11-24 04:35:17 +0000

    Add tests for client.get_armed_status

--- tests/test_client_misc.py
+++ tests/test_client_misc.py
@@ -4,7 +4,13 @@ import unittest
 from unittest.mock import patch
 
 from common import create_client
-from const import LOCATION_INFO_BASIC_NORMAL, RESPONSE_DISARMED
+from const import (
+    LOCATION_INFO_BASIC_NORMAL,
+    RESPONSE_ARMED_AWAY,
+    RESPONSE_ARMED_STAY,
+    RESPONSE_ARMED_STAY_NIGHT,
+    RESPONSE_DISARMED,
+)
 from TotalConnectClient import ZONE_STATUS_NORMAL
 
 
@@ -34,3 +40,36 @@ class TestTotalConnectClient(unittest.TestCase):
 
             # first ask for status of zone 99, which does not exist
             assert self.client.zone_status(self.location_id, "99") is None
+
+    def tests_get_armed_status(self):
+        """Test get_armed_status."""
+        responses = [
+            RESPONSE_ARMED_STAY,
+            RESPONSE_ARMED_STAY_NIGHT,
+            RESPONSE_ARMED_AWAY,
+            RESPONSE_DISARMED,
+        ]
+        with patch(
+            "TotalConnectClient.TotalConnectClient.request", side_effect=responses
+        ):
+            # should start disarmed
+            assert self.client.locations[self.location_id].is_disarmed() is True
+
+            # armed_stay
+            assert self.client.get_armed_status(self.location_id)
+            assert self.client.locations[self.location_id].is_armed_home() is True
+
+            # armed_stay_night
+            assert self.client.get_armed_status(self.location_id)
+            assert self.client.locations[self.location_id].is_armed_home() is True
+            assert self.client.locations[self.location_id].is_armed_night() is True
+
+            # armed_away
+            assert self.client.get_armed_status(self.location_id)
+            assert self.client.locations[self.location_id].is_armed_away() is True
+
+            # TODO: test alarming states
+
+            # disarmed
+            assert self.client.get_armed_status(self.location_id)
+            assert self.client.locations[self.location_id].is_disarmed() is True
