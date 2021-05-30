fd06e4be9c1090402675b809ae6c104acaff3745
Austin Mroczek <austin@mroczek.org>
2021-05-30 03:04:12 +0000

    create live_test_partitions.py

---
+++ total_connect_client/live_test_partitions.py
@@ -0,0 +1,28 @@
+"""Test your system from the command line."""
+
+import logging
+import sys
+from pprint import pprint
+
+import TotalConnectClient
+
+logging.basicConfig(filename="test.log", level=logging.DEBUG)
+
+if len(sys.argv) != 6:
+    print(
+        "usage:  python3 test.py username password DeviceID LocationID\n"
+    )
+    sys.exit()
+
+USERNAME = sys.argv[1]
+PASSWORD = sys.argv[2]
+LOCATION = sys.argv[3]
+DEVICE_ID = sys.argv[4]
+
+TC = TotalConnectClient.TotalConnectClient(USERNAME, PASSWORD)
+
+print("\n\n\nGetPartitionsDetails without partition number\n\n\n")
+
+result = TC.request(f"GetPartitionsDetails(self.token, {LOCATION}, {DEVICE_ID})")
+
+pprint(result)
