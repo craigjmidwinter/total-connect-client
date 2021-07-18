4cf0e14d4185be456d303c1a9407a372716c0193
dulitz <dulitz@gmail.com>
2021-07-18 15:08:36 -0700

    exceptions that can be thrown by public methods in this package

---
+++ total_connect_client/exceptions.py
@@ -0,0 +1,34 @@
+"""
+Exceptions raised by TotalConnectClient.
+"""
+
+
+class TotalConnectError(Exception):
+    pass
+
+
+class AuthenticationError(TotalConnectError):
+    pass
+
+
+class InvalidSessionError(AuthenticationError):
+    pass
+
+
+class BadResultCodeError(TotalConnectError):
+    pass
+
+
+class RetryableTotalConnectError(TotalConnectError):
+    """These errors are likely to resolve themselves if the action is retried.
+    If an error requires some other action (such as reauthenticating an
+    expired session) before being retried, it is not "retryable."
+    """
+    pass
+
+
+class PartialResponseError(RetryableTotalConnectError):
+    """Raised if the response is missing a section that it is always supposed to have.
+    Because the TotalConnect servers are flaky, these are rather frequent.
+    """
+    pass
