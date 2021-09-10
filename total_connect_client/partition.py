f77e4dfd13f31009ea0088a0dc88c484e41c14cb
Austin Mroczek <austin@mroczek.org>
2021-09-10 20:10:53 +0000

    grab more partition details

--- total_connect_client/partition.py
+++ total_connect_client/partition.py
@@ -17,11 +17,25 @@ class TotalConnectPartition:
         self.parent = parent
         self.partitionid = details.get("PartitionID")
         self.name = details.get("PartitionName")
+        self.is_stay_armed = details.get("IsStayArmed")
+        self.is_fire_enabled = details.get("IsFireEnabled")
+        self.is_common_enabled = details.get("IsCommonEnabled")
+        self.is_locked = details.get("IsLocked")
+        self.is_new_partition = details.get("IsNewPartition")
+        self.is_night_stay_enabled = details.get("IsNightStayEnabled")
+        self.exit_delay_timer = details.get("ExitDelayTimer")
         self._update(details)
 
     def __str__(self):
         """Return a string that is printable."""
-        data = f"PARTITION {self.partitionid} - {self.name}\n" f"  {self.arming_state}\n"
+        data = (
+            f"PARTITION {self.partitionid} - {self.name}\n" f"  {self.arming_state}\n"
+            f"  Stay armed: {self.is_stay_armed}\tFire enabled: {self.is_fire_enabled}\n"
+            f"  Common enabled: {self.is_common_enabled}\tLocked: {self.is_locked}\n"
+            f"  New: {self.is_new_partition}\tNight Stay enabled: {self.is_night_stay_enabled}\n"
+            f"  Exit delay: {self.exit_delay_timer}\n"
+        )
+        
         return data
 
     def arm(self, arm_type):
