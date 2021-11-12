a7c960046a70e109a20fe45b12018e3cda3fac98
Austin Mroczek <austin@mroczek.org>
2021-11-12 00:42:37 +0000

    bump version to 2021.11.3

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="total_connect_client",
-    version="2021.11.2",
+    version="2021.11.3",
     description="Interact with Total Connect 2 alarm systems",
     author="Craig J. Midwinter",
     author_email="craig.j.midwinter@gmail.com",
