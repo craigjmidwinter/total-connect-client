0cc0d176fde42d3b6cda0ef3fc97da0ff9d68ae6
Austin Mroczek <austin@mroczek.org>
2021-11-21 16:21:41 +0000

    update command line disarm

--- total_connect_client/disarm.py
+++ total_connect_client/disarm.py
@@ -1,5 +1,6 @@
 """Test your system from the command line."""
 
+import getpass
 import logging
 import sys
 
@@ -7,20 +8,19 @@ from total_connect_client.client import TotalConnectClient
 
 logging.basicConfig(filename="test.log", level=logging.DEBUG)
 
-if len(sys.argv) != 4:
-    print("usage:  username password " "location1=usercode1,location2=usercode2 \n")
+if len(sys.argv) != 3:
+    print("usage:  username location1=usercode1,location2=usercode2 \n")
     sys.exit()
 
 USERNAME = sys.argv[1]
-PASSWORD = sys.argv[2]
-USERCODES = dict(x.split("=") for x in sys.argv[3].split(","))
+USERCODES = dict(x.split("=") for x in sys.argv[2].split(","))
+PASSWORD = getpass.getpass()
 
 TC = TotalConnectClient(USERNAME, PASSWORD, USERCODES)
 
 for location_id in TC.locations:
-    if TC.disarm(location_id):
-        print("Disarm success.")
-    else:
-        print("Disarm failure.  Check logs.")
+    TC.locations[location_id].disarm()
+
+print("Disarmed.")
 
 sys.exit()
